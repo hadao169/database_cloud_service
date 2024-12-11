ed156a5110623a8f4028ace8a873697dd7750f76
Dao Thai Ha <daothaiha1692005@gmail.com>
2024-12-11 05:07:23 +0200

    latest commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 // Connect to SQLite database
-const db = new sqlite3.Database('./db.sqlite3', (err) => {
+const dbPath = process.env.DATABASE_PATH || "./my_finance_tracking_app.db"; // Default to local if not set
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
-    console.error('Could not connect to SQLite database', err);
+    console.error("Could not connect to SQLite database", err);
   } else {
-    console.log('Connected to SQLite database.');
+    console.log("Connected to SQLite database.");
   }
 });
 
+db.serialize(() => {
+  db.each("SELECT rowid AS id, info FROM user_info", (err, row) => {
+    if (err) {
+      console.error("Error fetching data", err);
+    } else {
+      console.log(`Row ${row.id}: ${row.info}`);
+    }
+  });
+});
